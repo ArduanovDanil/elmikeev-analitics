515bf520d4425413b854af26bb508b91140c416f
Arduanov Danil <rylezz@gmail.com>
2025-10-04 12:30:24 +0500

    Create Income

---
+++ app/Console/Commands/ParseApi/Income.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands\ParseApi;
+
+use Illuminate\Console\Command;
+
+class Income extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:income';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
